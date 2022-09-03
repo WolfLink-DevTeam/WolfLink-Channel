9a17fc141bf1433077b63182a4fa92fe046acecd
刀哥 <3523206925@qq.com>
2022-09-03 17:57:39 +0800

    add scope

--- VanillaGlobalChannel-SpringBoot/VGC-SpringBoot-服务端整合包/双击启动.bat
+++ VanillaGlobalChannel-SpringBoot/VGC-SpringBoot-服务端整合包/双击启动.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk-17.0.2\bin\java.exe" -jar VanillaGlobalChannel-1.0.1-Beta.jar --spring.config.location=%cd%\application.yml
+"C:\Program Files\Java\jdk-17.0.4\bin\java.exe" -jar VanillaGlobalChannel-1.0.1-Beta.jar --spring.config.location=%cd%\application.yml
