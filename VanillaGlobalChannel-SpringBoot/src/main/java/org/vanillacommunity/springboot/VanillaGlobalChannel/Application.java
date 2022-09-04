6db12b530333750d48ce3e9660ea5ab83d6e3182
刀哥 <3523206925@qq.com>
2022-09-04 15:46:33 +0800

    heartbeat

--- VanillaGlobalChannel-SpringBoot/src/main/java/org/vanillacommunity/springboot/VanillaGlobalChannel/Application.java
+++ VanillaGlobalChannel-SpringBoot/src/main/java/org/vanillacommunity/springboot/VanillaGlobalChannel/Application.java
@@ -3,6 +3,7 @@ package org.vanillacommunity.springboot.VanillaGlobalChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.vanillacommunity.springboot.VanillaGlobalChannel.controller.WSController;
 
 
@@ -19,6 +20,7 @@ public class Application {
 
 	public static ChannelManager channelManager;
 
+//	public static SessionHeartBeatTask heartBeatTask;
 
 	public static void main(String[] args) {
 		appContext = SpringApplication.run(Application.class, args);
@@ -27,6 +29,7 @@ public class Application {
 		wsController = (WSController) appContext.getBean("wsController");
 		wsManager = (WSManager) appContext.getBean("wsManager");
 		channelManager = (ChannelManager) appContext.getBean("channelManager");
+//		heartBeatTask = (SessionHeartBeatTask) appContext.getBean("heartBeatTask");
 
 		mcServerManager.init();
 		channelManager.init();
