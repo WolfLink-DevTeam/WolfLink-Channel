7b8cc06b9b6d9ea10ae59adec26fe3f6dfbedbd7
MikkoAyaka <3401286177@qq.com>
2023-06-12 22:08:48 +0800

    更新 VGC 服务端整合包

--- VanillaGlobalChannel-SpringBoot/VGC-SpringBoot-服务端整合包/双击启动.bat
+++ VanillaGlobalChannel-SpringBoot/VGC-SpringBoot-服务端整合包/双击启动.bat
@@ -1,2 +1,2 @@
-"D:\Environment\Oracle-JDK-17\bin\java.exe" -jar VanillaGlobalChannel-1.0.1-Beta.jar --spring.config.location=%cd%\application.yml
+"D:\Environment\Oracle-JDK-17\bin\java.exe" -jar VanillaGlobalChannel-1.0.1.jar --spring.config.location=%cd%\application.yml
 pause
