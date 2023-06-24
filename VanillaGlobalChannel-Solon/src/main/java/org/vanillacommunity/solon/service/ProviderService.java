158c4cc9623ed57ddd9f9577b262ea050b5ef217
MikkoAyaka <3401286177@qq.com>
2023-06-24 11:05:31 +0800

    补充 ProviderService 的日志信息

--- VanillaGlobalChannel-Solon/src/main/java/org/vanillacommunity/solon/service/ProviderService.java
+++ VanillaGlobalChannel-Solon/src/main/java/org/vanillacommunity/solon/service/ProviderService.java
@@ -5,6 +5,7 @@ import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Singleton;
 import org.noear.solon.core.message.Session;
+import org.vanillacommunity.solon.Logger;
 import org.vanillacommunity.solon.entity.provider.OnlineProvider;
 import org.vanillacommunity.solon.entity.provider.Provider;
 import org.vanillacommunity.solon.repository.ProviderRepository;
@@ -16,7 +17,8 @@ import java.util.Date;
 public class ProviderService {
     @Inject
     ProviderRepository providerRepository;
-
+    @Inject
+    Logger logger;
     public void login(Session session) {
         @NonNull
         String account = session.param("account");
@@ -24,6 +26,7 @@ public class ProviderService {
         String token = session.param("token");
         int channelId = Integer.parseInt(session.param("channel_id"));
         providerRepository.update(new OnlineProvider(account, token, channelId, session, new Date()));
+        logger.info(account+" 成功登录，所在频道 "+channelId);
     }
 
     public void logout(Session session) {
@@ -31,7 +34,9 @@ public class ProviderService {
         String account = session.param("account");
         @NonNull
         String token = session.param("token");
+        int channelId = Integer.parseInt(session.param("channel_id"));
         providerRepository.update(new Provider(account, token));
+        logger.info(account+" 已离线，所在频道 "+channelId);
     }
 
 }
