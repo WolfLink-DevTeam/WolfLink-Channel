7787da2953ccd9c8c682687a6222221b2ae46a16
Amog_ <1356492075@qq.com>
2023-09-11 12:51:29 +0800

    补充完善客户端，频道，在线客户端的接口

--- Solon/src/main/java/org/vanillacommunity/solon/controller/HttpController.java
+++ Solon/src/main/java/org/vanillacommunity/solon/controller/HttpController.java
@@ -2,9 +2,15 @@ package org.vanillacommunity.solon.controller;
 
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
+import org.vanillacommunity.solon.entity.OnlineClient;
+import org.vanillacommunity.solon.repository.OnlineClientRepository;
+import org.vanillacommunity.solon.repository.SecureChannelRepository;
 import org.vanillacommunity.solon.repository.SecureClientRepository;
 import org.wolflink.common.ioc.IOC;
+import org.wolflink.minecraft.Channel;
 import org.wolflink.minecraft.Client;
+
+import java.util.Set;
 
 @Controller
 @Mapping("/query")
@@ -12,5 +18,15 @@ public class HttpController {
     @Mapping("/client")
     public Client queryClient(String client_account) {
         return IOC.getBean(SecureClientRepository.class).find(client_account);
+    }
+
+    @Mapping("/channel")
+    public Channel queryChannel(int channel_id) {
+        return IOC.getBean(SecureChannelRepository.class).find(channel_id);
+    }
+
+    @Mapping("/channel_online_clients")
+    public Set<OnlineClient> queryChannel_online_clients(int channel_id) {
+        return IOC.getBean(OnlineClientRepository.class).filterByChannelId(channel_id);
     }
 }
